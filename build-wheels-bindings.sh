94ce714a84e81d18606b578eba22f3d7637b59aa
Christophe Guillon <christophe.guillon@inria.fr>
2025-02-13 19:36:11 +0100

    Install bindings components only for mlir whels bindings

--- build-wheels-bindings.sh
+++ build-wheels-bindings.sh
@@ -16,14 +16,12 @@ env \
     CIBW_PROJECT_REQUIRES_PYTHON='>=3.10' \
     CIBW_MANYLINUX_X86_64_IMAGE='manylinux_2_28' \
     CIBW_BEFORE_ALL='./install-build-tools.sh && ./update-ccache-from-host.sh' \
-    CIBW_BEFORE_BUILD='rm -rf python_bindings/mlir python_bindings/dist python_bindings/build python_bindings/*egg-info && env BUILD_LLVM_MLIR_BINDINGS=1 BUILD_LLVM_TOOLS=0 BUILD_LLVM_CCACHE=1 ./build-mlir-bindings.sh && mv install-bindings/mlir python_bindings/' \
+    CIBW_BEFORE_BUILD='rm -rf python_bindings/mlir python_bindings/dist python_bindings/build python_bindings/*egg-info && env BUILD_LLVM_COMPONENTS=mlir_bindings BUILD_LLVM_MLIR_BINDINGS=1 BUILD_LLVM_TOOLS=0 BUILD_LLVM_CCACHE=1 ./build-mlir-bindings.sh && mv install-bindings/mlir python_bindings/' \
     CIBW_TEST_COMMAND='{package}/test-installed.sh' \
     BUILD_LLVM_REVISION="$BUILD_LLVM_REVISION" \
     CIBW_ENVIRONMENT_PASS_LINUX="BUILD_LLVM_REVISION" \
-    CIBW_DEBUG_KEEP_CONTAINER=1 \
     CIBW_BUILD_VERBOSITY=1 \
     cibuildwheel \
     python_bindings
 
-#    CIBW_DEBUG_KEEP_CONTAINER=1 \
 #    CIBW_BUILD_VERBOSITY=1 \
