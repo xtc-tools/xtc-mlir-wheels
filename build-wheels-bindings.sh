d89369d229efa6dcd06c51f91be1b2052704ee00
Christophe Guillon <christophe.guillon@inria.fr>
2025-10-02 18:05:14 +0200

    build: use ccache by default and do not clean build dir after build

--- build-wheels-bindings.sh
+++ build-wheels-bindings.sh
@@ -7,6 +7,9 @@ BUILD_LLVM_REVISION="$(cat "$dir"/llvm_revision.txt)"
 
 cd "$dir"
 
+BUILD_LLVM_CLEAN_BUILD_DIR="${BUILD_LLVM_CLEAN_BUILD_DIR:-1}"
+CIBW_DEBUG_KEEP_CONTAINER="${CIBW_DEBUG_KEEP_CONTAINER:-0}"
+
 # Build bindings, first fill cache, then build bindings per python version
 # Note that there is an issue with python 3.13, skipped
 env \
@@ -16,11 +19,13 @@ env \
     CIBW_PROJECT_REQUIRES_PYTHON='>=3.10' \
     CIBW_MANYLINUX_X86_64_IMAGE='manylinux_2_28' \
     CIBW_BEFORE_ALL='./install-build-tools.sh && ./update-ccache-from-host.sh' \
-    CIBW_BEFORE_BUILD='rm -rf python_bindings/mlir python_bindings/dist python_bindings/build python_bindings/*egg-info && env BUILD_LLVM_COMPONENTS=mlir_bindings BUILD_LLVM_MLIR_BINDINGS=1 BUILD_LLVM_TOOLS=0 BUILD_LLVM_CCACHE=1 ./build-mlir-bindings.sh && mv install-bindings/mlir python_bindings/' \
+    CIBW_BEFORE_BUILD='rm -rf python_bindings/mlir python_bindings/dist python_bindings/build python_bindings/*egg-info && env BUILD_LLVM_COMPONENTS=mlir_bindings BUILD_LLVM_MLIR_BINDINGS=1 BUILD_LLVM_TOOLS=0 ./build-mlir-bindings.sh && mv install-bindings/mlir python_bindings/' \
     CIBW_TEST_COMMAND='{package}/test-installed.sh' \
     BUILD_LLVM_REVISION="$BUILD_LLVM_REVISION" \
-    CIBW_ENVIRONMENT_PASS_LINUX="BUILD_LLVM_REVISION" \
+    BUILD_LLVM_CLEAN_BUILD_DIR="$BUILD_LLVM_CLEAN_BUILD_DIR" \
+    CIBW_ENVIRONMENT_PASS_LINUX="BUILD_LLVM_CLEAN_BUILD_DIR BUILD_LLVM_REVISION" \
     CIBW_BUILD_VERBOSITY=1 \
+    CIBW_DEBUG_KEEP_CONTAINER="$CIBW_DEBUG_KEEP_CONTAINER" \
     cibuildwheel \
     python_bindings
 
