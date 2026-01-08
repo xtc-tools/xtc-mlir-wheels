bbd08db6d969b1888cad8d89df3da8ef1e6c4b6b
Sylvain Noiry <sylvain.noiry@inria.fr>
2026-01-08 18:49:29 +0100

    Create a mlir-dev package
    This package contains all the necessary elements to build an out-of-tree
    project, without recompiling llvm. It is compiled with the same
    configuration as mlir.

---
+++ mlir_dev/setup.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from setuptools import setup
+from setuptools.dist import Distribution
+from setuptools.command.bdist_wheel import bdist_wheel
+
+
+PACKAGE_NAME = "mlir-dev"
+PACKAGE_VERSION = "21.1.2.2025091603+b708aea0"
+
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(foo):
+        return True
+
+
+class CBdistWheel(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+        # Force python and abi to generic python3
+        return "py3", "none", plat
+
+
+if __name__ == "__main__":
+
+    # Create the binary distribution
+    setup(
+        name = PACKAGE_NAME,
+        version = PACKAGE_VERSION,
+        description = "Python packaging for mlir tools and mlir dev files",
+        maintainer="Christophe Guillon",
+        maintainer_email="christophe.guillon@inria.fr",
+        python_requires = ">= 3.10",
+        packages = ["mlir_dev"],
+        package_dir = {"mlir_dev": "install"},
+        include_package_data = True,
+        distclass=BinaryDistribution,
+        cmdclass={
+            "bdist_wheel": CBdistWheel,
+        },
+        setup_requires = [
+            "setuptools>=80",
+        ],
+        zip_safe = False
+    )
