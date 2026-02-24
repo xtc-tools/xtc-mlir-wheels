6ed117c73bf669780eafc05e15444c0dca6c964f
Christophe Guillon <christophe.guillon@inria.fr>
2026-02-24 12:44:00 +0100

    Fix test for macOS

--- mlir-tools/test-installed.sh
+++ mlir-tools/test-installed.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 set -x
 
 PREFIX="$(python -c 'import mlir;print(mlir.__path__[0])')"
-[ -f "$PREFIX"/lib/libMLIR.so ] || exit 1
-[ -f "$PREFIX"/lib/libMLIR-C.so ] || exit 1
 
 "$PREFIX"/bin/mlir-opt --version
 
