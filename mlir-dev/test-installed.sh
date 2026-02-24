041f89a5924f303d1943ce1ab68b740aa6107364
Christophe Guillon <christophe.guillon@inria.fr>
2026-02-24 15:24:04 +0100

    Update tests

--- mlir-dev/test-installed.sh
+++ mlir-dev/test-installed.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 set -x
 
 PREFIX="$(python -c 'import mlir;print(mlir.__path__[0])')"
-[ -d "$PREFIX/include" ] || exit 1
+[ -d "$PREFIX/include/mlir" ] || exit 1
 [ -d "$PREFIX/lib/cmake/mlir" ] || exit 1
 
 echo "ALL TESTS PASSED"
